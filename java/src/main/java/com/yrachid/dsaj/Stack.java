891befbcf432ed4cbf94c46afbf3c2e6a0551dfc
yrachid <anuar.yasser@gmail.com>
2021-01-31 17:12:29 -0300

    Renames Stack currentSize to top because it's a much better name

--- java/src/main/java/com/yrachid/dsaj/Stack.java
+++ java/src/main/java/com/yrachid/dsaj/Stack.java
@@ -3,25 +3,25 @@ package com.yrachid.dsaj;
 public class Stack {
 
     private final int maxSize;
-    private int currentSize;
+    private int top;
     private long[] items;
 
     public Stack(int maxSize) {
         this.maxSize = maxSize;
-        this.currentSize = 0;
+        this.top = 0;
         this.items = new long[maxSize];
     }
 
     public void push(long element) {
-        if (currentSize  == maxSize) {
+        if (top == maxSize) {
             throw new StackOverflowException("Max stack size exceeded");
         }
 
-        items[currentSize++] = element;
+        items[top++] = element;
     }
 
     public long pop() {
-        return items[--currentSize];
+        return items[--top];
     }
 
     public static class StackOverflowException extends RuntimeException {
